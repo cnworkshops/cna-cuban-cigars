353acda36d6ab4faaef0a8fba8f29cb9a0b32de0
ampacheco <alejandropacheco@hotmail.com>
2020-01-05 23:29:48 -0300

    Initial commit

---
+++ src/main/java/io/dromosa/cnacubancigars/CnaCubanCigarsApplication.java
@@ -0,0 +1,13 @@
+package io.dromosa.cnacubancigars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnaCubanCigarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnaCubanCigarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dromosa/cnacubancigars/CnaCubanCigarsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dromosa.cnacubancigars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnaCubanCigarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
